1d1459177ebb00f88fc815e38aeec8627e82f652
kacper <kacpergorec@gmail.com>
2022-03-22 22:53:20 +0100

    Added a MarkdownHelper Service for handlig markdown syntax

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,12 +5,11 @@ namespace App\Controller;
 
 
 use App\Entity\Question;
+use App\Service\MarkdownHelper;
 use Doctrine\ORM\EntityManagerInterface;
-use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
 
 class QuestionController extends AbstractController
@@ -63,7 +62,7 @@ EOF
     /**
      * @Route("/questions/{slug}",name="app_question_show")
      */
-    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache)
+    public function show($slug, MarkdownHelper $markdownHelper)
     {
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still ?',
@@ -73,10 +72,7 @@ EOF
 
         $questionText = "I've been turned into a cat, any thoughts on how to turn back? While I'm **adorable**, I don't really care for cat food.";
 
-        dump($cache);
-        $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function() use ($markdownParser,$questionText){
-            return $markdownParser->transformMarkdown($questionText);
-        });
+        $parsedQuestionText = $markdownHelper->parse($questionText);
 
         return $this->render('questions/show.html.twig', [
             'question' => ucfirst(str_replace('-', ' ', $slug)),
